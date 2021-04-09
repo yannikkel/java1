a50768516c5441632da38e0ecbb581ced6464683
yannikkel <78969123+yannikkel@users.noreply.github.com>
2021-04-09 13:10:02 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,18 @@
-int customerAccount = 100;
+public class Main {
+    public static void main(String[] args) {
+        int customerAccount = 1200;
         int bonusScale = 100;
-        int uploadVolume = 1200;
-        int bonus;
+        int uploadVolume = 17000;
         int bonus = 0;
 
         if (uploadVolume >= 1000){
             bonus = uploadVolume / bonusScale;
-        } else {
-            bonus = 0;
         }
 
         int totalAccount = customerAccount + uploadVolume + bonus;
+
+        System.out.println("Вы пополнили счёт на " + uploadVolume + " руб.");
+        System.out.println("Ваш бонус составил " + bonus + " руб.");
+        System.out.println("Ваш баланс " + totalAccount + " руб.");
+    }
+}
